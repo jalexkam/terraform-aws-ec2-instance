5fb6b89398dd213242e25fb73b437db5a787c46a
Alexandre <jalex.kam@gmail.com>
2025-02-18 23:59:12 +0000

    new tag

--- main.tf
+++ main.tf
@@ -607,16 +607,3 @@ resource "aws_iam_instance_profile" "this" {
 ################################################################################
 # Elastic IP
 ################################################################################
-
-resource "aws_eip" "this" {
-  count = local.create && var.create_eip && !var.create_spot_instance ? 1 : 0
-
-  instance = try(
-    aws_instance.this[0].id,
-    aws_instance.ignore_ami[0].id,
-  )
-
-  domain = var.eip_domain
-
-  tags = merge(var.tags, var.eip_tags)
-}
